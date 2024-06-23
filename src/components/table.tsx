a46c43d643f07fe9116cf9d18b48d84908d40a1e
Kellie Kumasaka <kelliekumasaka@Kellies-MacBook-Pro.local>
2024-06-22 22:39:09 -0700

    remove unused import

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material'
 
 enum AppliedOn {
